f6ca32f9328073096b16af44397bab4dc5e3f415
austinameh007 <42697938+austinameh007@users.noreply.github.com>
2018-08-25 15:53:39 +0100

    Add files via upload

---
+++ Prinnt.js
@@ -0,0 +1,6 @@
+function adProceedOne(){
+window.setTimeout('window.open("http://www.mickysblog.com")', 1000);
+}
+function adProceedTwo(){
+window.setTimeout('window.open("https://www.facebook.com/Skytech-Investments-And-Technologies-Limited-2000249650233423/")', 1000);
+}
